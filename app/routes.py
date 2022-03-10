0cceec1808af9616c165650a0e2c3e440e2fb705
Cameron Hanna <channa@localhost.localdomain>
2022-03-09 22:53:58 -0600

    Lab 5 Part B

--- app/routes.py
+++ app/routes.py
@@ -57,6 +57,6 @@ def loginAPI():
         password = json_data["password"]
     else:
         return jsonify(Success=False)
-    if username == 'byan' and password == '123':
+    if username == 'channa2' and password == '567':
         return jsonify(Success=True, uid=11)
     return jsonify(Success=False)
